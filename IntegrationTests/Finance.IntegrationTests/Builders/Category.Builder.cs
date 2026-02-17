86f7e4648ae7641563a621952f7ebbb124c330e1
Abhiram13 <abhiramnagadi@gmail.com>
2026-02-17 18:04:49 +0530

    Inserting category in DB in integration tests

--- IntegrationTests/Finance.IntegrationTests/Builders/Category.Builder.cs
+++ IntegrationTests/Finance.IntegrationTests/Builders/Category.Builder.cs
@@ -1,6 +1,30 @@
+using BudgetTracker.Finance;
+using BudgetTracker.Finance.Entities;
+
 namespace IntegrationTests.Builders;
 
 public sealed class CategoryBuilder
 {
-    
+    private readonly WriteDbContext _dbContext;
+
+    public CategoryBuilder(WriteDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<Category> CreateCategoryAsync()
+    {
+        DateOnly date = DateOnly.FromDateTime(DateTime.Now);
+        Category category = new Category
+        {
+            Name = "Test category",
+            CreatedAt = date,
+            UpdatedAt = date,
+        };
+        
+        await _dbContext.Categories.AddAsync(category);
+        await _dbContext.SaveChangesAsync();
+        
+        return category;
+    }
 }
