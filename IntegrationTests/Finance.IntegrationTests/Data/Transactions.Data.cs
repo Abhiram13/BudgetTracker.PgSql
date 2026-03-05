d8eb918be56039d2357704f1cb317de82e76e1cf
Abhiram13 <abhiramnagadi@gmail.com>
2026-03-05 17:01:09 +0530

    Member data for happy path tests

--- IntegrationTests/Finance.IntegrationTests/Data/Transactions.Data.cs
+++ IntegrationTests/Finance.IntegrationTests/Data/Transactions.Data.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using BudgetTracker.Finance.Enums;
+using BudgetTracker.Finance.Models;
 using IntegrationTests.Definations.Transactions;
 
 namespace IntegrationTests.Data.Transactions;
@@ -7,6 +9,8 @@ public abstract class TheoryTestData<Def> : TheoryData<Def> where Def : class
 {
     protected readonly DateOnly _currentDate = DateOnly.FromDateTime(DateTime.UtcNow);
 }
+
+#region Insert Transactions
 
 public class TransactionsInsertDateValidationTestData : TheoryTestData<InsertTransactionDateDef>
 {
@@ -40,3 +44,110 @@ public class TransactionsInsertDateValidationTestData : TheoryTestData<InsertTra
         });
     }
 }
+
+public static class InsertTransactionsMemberTestData
+{
+    public static IEnumerable<object[]> HappyPathData()
+    {
+        DateOnly currentDate = DateOnly.FromDateTime(DateTime.UtcNow);
+        DateOnly pastDate = currentDate.AddDays(-10);
+        
+        // Debit, category id, from bank, current date
+        yield return new object[]
+        {
+            new InsertTransactionDto
+            {
+                ActualAmount = 200,
+                Amount = 200,
+                CategoryId = 1,
+                Description = "First Transaction #1",
+                Type = TransactionType.Debit,
+                FromBank = 1,
+                ToBank = null,
+                Date = currentDate,
+            }
+        };
+        
+        // Debit, category id, to bank, current date
+        yield return new object[]
+        {
+            new InsertTransactionDto
+            {
+                ActualAmount = 200,
+                Amount = 200,
+                CategoryId = 1,
+                Description = "First Transaction #1",
+                Type = TransactionType.Debit,
+                FromBank = null,
+                ToBank = 1,
+                Date = currentDate,
+            }
+        };
+        
+        // Debit, category id, from bank, past date
+        yield return new object[]
+        {
+            new InsertTransactionDto
+            {
+                ActualAmount = 200,
+                Amount = 200,
+                CategoryId = 1,
+                Description = "First Transaction #1",
+                Type = TransactionType.Debit,
+                FromBank = 1,
+                ToBank = null,
+                Date = pastDate,
+            }
+        };
+        
+        // Credit, category id, from bank, current date
+        yield return new object[]
+        {
+            new InsertTransactionDto
+            {
+                ActualAmount = 200,
+                Amount = 200,
+                CategoryId = 1,
+                Description = "First Credit Transaction #1",
+                Type = TransactionType.Credit,
+                FromBank = 1,
+                ToBank = null,
+                Date = currentDate,
+            }
+        };
+        
+        // Credit, category id, to bank, current date
+        yield return new object[]
+        {
+            new InsertTransactionDto
+            {
+                ActualAmount = 200,
+                Amount = 200,
+                CategoryId = 1,
+                Description = "First Credit Transaction #1",
+                Type = TransactionType.Credit,
+                FromBank = null,
+                ToBank = 1,
+                Date = currentDate,
+            }
+        };
+        
+        // Credit, category id, from bank, past date
+        yield return new object[]
+        {
+            new InsertTransactionDto
+            {
+                ActualAmount = 200,
+                Amount = 200,
+                CategoryId = 1,
+                Description = "First Credit Transaction #1",
+                Type = TransactionType.Credit,
+                FromBank = 1,
+                ToBank = null,
+                Date = pastDate,
+            }
+        };
+    }
+}
+
+#endregion
