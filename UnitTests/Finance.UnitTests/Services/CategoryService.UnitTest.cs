7ff090525fe44aca4b28919101a7ca852a9b595b
Abhiram13 <abhiramnagadi@gmail.com>
2026-03-13 22:19:07 +0530

    unit tests

---
+++ UnitTests/Finance.UnitTests/Services/CategoryService.UnitTest.cs
@@ -0,0 +1,51 @@
+using BudgetTracker.Finance.Entities;
+using BudgetTracker.Finance.Interfaces;
+using BudgetTracker.Finance.Repository;
+using BudgetTracker.Finance.Services;
+using BudgetTracker.Shared.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Moq;
+
+namespace UnitTests.Finance.Services;
+
+public class CategoryServiceUnitTests
+{
+    private readonly Mock<ICategoryRepository> _categoryRepository = new Mock<ICategoryRepository>();
+    private readonly CategoryService _categoryService;
+
+    public CategoryServiceUnitTests()
+    {
+        _categoryService = new CategoryService(_categoryRepository.Object);
+    }
+    
+    [Fact]
+    public async Task GetCategoryById_Success_Async()
+    {
+        // Arrange
+        Category category = new Category { Id = 1, Name = "Food" };
+        _categoryRepository.Setup(r => r.SearchByIdAsync(1)).ReturnsAsync(category);
+
+        // Act
+        Category result = await _categoryService.GetCategoryByIdAsync(1);
+
+        // Assert
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Id);
+        Assert.Equal("Food", result.Name);
+    }
+    
+    [Fact]
+    public async Task GetCategoryById_Throws_Error_Async()
+    {
+        _categoryRepository
+            .Setup(r => r.SearchByIdAsync(0))
+            .ThrowsAsync(new BadHttpRequestException("Category not found"));
+
+        // Act
+        Func<Task<Category>> action = () => _categoryService.GetCategoryByIdAsync(0);
+
+        // Assert
+        Exception error = await Assert.ThrowsAsync<InvalidPayloadException>(action);
+        Assert.Equal("Category not found", error.Message);
+    }
+}
