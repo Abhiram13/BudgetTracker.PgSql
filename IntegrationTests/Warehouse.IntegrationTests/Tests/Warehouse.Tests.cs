2b23f524d6d33f5736ce4b6119f57ba621a2936c
Abhiram13 <abhiramnagadi@gmail.com>
2026-03-14 20:26:17 +0530

    removed BUG comment, new asserts in big query test

--- IntegrationTests/Warehouse.IntegrationTests/Tests/Warehouse.Tests.cs
+++ IntegrationTests/Warehouse.IntegrationTests/Tests/Warehouse.Tests.cs
@@ -38,5 +38,8 @@ public class BigQueryTests : IClassFixture<WarehouseTestWebApplicationFactory>
         Assert.NotNull(apiResponse.Result);
         Assert.True(apiResponse.Result.Count > 0);
         Assert.Equal(100, apiResponse.Result[0].Credit);
+        Assert.Equal(1000, apiResponse.Result[0].Debit);
+        Assert.Equal(DateOnly.FromDateTime(DateTime.Now), apiResponse.Result[0].Date);
+        Assert.Equal(10, apiResponse.Result[0].Count);
     }
 }
