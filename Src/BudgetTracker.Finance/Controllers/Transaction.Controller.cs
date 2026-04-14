d8219cea30e2b78e9453bd41efdd9f168edb4d51
Abhiram13 <abhiramnagadi@gmail.com>
2026-04-14 20:56:59 +0530

    Dotnet 10, Swagger XML documentation

--- Src/BudgetTracker.Finance/Controllers/Transaction.Controller.cs
+++ Src/BudgetTracker.Finance/Controllers/Transaction.Controller.cs
@@ -1,29 +1,66 @@
+using System.Data.Entity.Infrastructure;
 using BudgetTracker.Finance.Services;
 using BudgetTracker.Finance.Entities;
 using BudgetTracker.Finance.Models;
+using BudgetTracker.Shared.Exceptions;
 using BudgetTracker.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
 using BudgetTracker.Shared.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using BudgetTracker.Shared.Security;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace BudgetTracker.Finance.Controllers;
 
+/// <summary>
+/// Provides endpoints for managing and retrieving transactions.
+/// </summary>
+/// <remarks>
+/// All requests to this controller require a valid <c>YARP_API_KEY</c> provided in the request headers.
+/// </remarks>
 [ApiController]
 [Authorize(AuthenticationSchemes = YarpApiKeySchemaOptions.DefaultSchema)]
 [Route("api/transactions")]
+[Produces("application/json")]
+[Consumes("application/json")]
 public class TransactionController : ControllerBase
 {
     private readonly TransactionService _transactionService;
     private readonly TraceIdProvider _traceProvider;
 
+    /// <summary>
+    /// Initializes the controller that handles all incoming web requests for transaction APIs.
+    /// </summary>
+    /// <param name="transactionService"><see cref="TransactionService"/> - The service that handles the actual business logic.</param>
+    /// <param name="trace"><see cref="TraceIdProvider"/> - A provider that gives a unique GUID for each request, used to track logs and find bugs.</param>
     public TransactionController(TransactionService transactionService, TraceIdProvider trace)
     {
         _transactionService = transactionService;
         _traceProvider = trace;
     }
 
+    /// <summary>
+    /// Inserts a new transaction.
+    /// </summary>
+    /// <param name="payload">The transaction details including amount, source, and metadata.</param>
+    /// <returns>A response containing the ID and status of the inserted transaction.</returns>
+    /// <response code="201">Returns the created transaction details</response>
+    /// <response code="400">
+    /// Returns when Payload is invalid like, date is future or in invalid format, amount is not in range or invalid bank or category ID provided.
+    /// </response>
+    /// <response code="500">
+    /// Returns when any internal exception or DB updates failed due to constraints violations.
+    /// </response>
+    /// <exception cref="InvalidPayloadException">
+    /// Thrown when <paramref name="payload"/> fails validation (e.g., negative amounts, missing required fields, future dates).
+    /// </exception>
+    /// <exception cref="DbUpdateException">
+    /// Thrown if the transaction cannot be inserted in the database, due to any constraint violations
+    /// </exception>
     [HttpPost]
+    [ProducesResponseType(typeof(ApiResponse<InsertTransactionResponseDto>), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult<ApiResponse<InsertTransactionResponseDto>>> InsertAsync([FromBody] InsertTransactionDto payload)
     {
         InsertTransactionResponseDto response = await _transactionService.InsertTransactionAsync(payload);
@@ -35,8 +72,20 @@ public class TransactionController : ControllerBase
             Result = response
         });
     }
+    
 
+    /// <summary>
+    /// Retrieves all transactions recorded on a specific date. Can return empty <see cref="TransactionByDateDto"/> if none found.
+    /// </summary>
+    /// <param name="date">The date string (e.g., <c>yyyy-MM-dd</c>) to filter.</param>
+    /// <exception cref="InvalidDateException">Thrown when given date is not in <c>yyyy-MM-dd</c> format or if future date is given</exception>
+    /// <response code="200">Returns list of transactions based on given date</response>
+    /// <response code="400">Returns when date is in invalid format or future.</response>
+    /// <response code="500">Returns when any internal exception or DB updates failed due to constraints violations.</response>
     [HttpGet("date/{date}")]
+    [ProducesResponseType(typeof(ApiResponse<TransactionByDateDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult<ApiResponse<TransactionByDateDto>>> GetTransactionsByDateAsync([FromRoute] string date)
     {
         TransactionByDateDto result = await _transactionService.GetTransactionsByDateAsync(date);
@@ -48,7 +97,19 @@ public class TransactionController : ControllerBase
         });
     }
 
+    /// <summary>
+    /// Counts total transactions for given month and year
+    /// </summary>
+    /// <param name="month">Optional month (1-12) to filter the count. Defaults to current month</param>
+    /// <param name="year">Optional year to filter the count. Defaults to current year</param>
+    /// <exception cref="InvalidPayloadException">Thrown when given month or year is greater than current month and year</exception>
+    /// <response code="200">Returns total count of transactions based on given month and year</response>
+    /// <response code="400">Returns when given month or year is in invalid format or future.</response>
+    /// <response code="500">Returns when any internal exception or DB updates failed due to constraints violations.</response>
     [HttpGet("count")]
+    [ProducesResponseType(typeof(ApiResponse<TransactionByDateDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> CountOfTransactionsAsync([FromQuery] int? month, [FromQuery] int? year)
     {
         int count = await _transactionService.CountOfAllTransactionsAsync(month, year);
@@ -61,7 +122,20 @@ public class TransactionController : ControllerBase
         });
     }
 
+    /// <summary>
+    /// Updates an existing transaction.
+    /// </summary>
+    /// <remarks>All the values provided will be replaced</remarks>
+    /// <param name="payload">The <see cref="UpdateTransactionDto"/> containing updated values</param>
+    /// <param name="id">The ID of the transaction to update.</param>
+    /// <exception cref="BadHttpRequestException">Thrown if no transaction exists with the provided <paramref name="id"/>.</exception>
+    /// <response code="200">Returns when a transaction is successfully updated</response>
+    /// <response code="400">Returns any of the payload values are invalid like date is in wrong format or category, bank ids are invalid.</response>
+    /// <response code="500">Returns when any internal exception or DB updates failed due to constraints violations.</response>
     [HttpPut("{id}")]
+    [ProducesResponseType(typeof(ApiResponse<TransactionByDateDto>), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ApiResponse<object>), StatusCodes.Status500InternalServerError)]
     public async Task<IActionResult> UpdateTransactionAsync([FromRoute] int id, [FromBody] UpdateTransactionDto payload)
     {
         await _transactionService.UpdateTransactionAsync(payload, id);
--- Src/BudgetTracker.Shared/Models/ApiResponse.Model.cs
+++ Src/BudgetTracker.Shared/Models/ApiResponse.Model.cs
@@ -2,18 +2,39 @@ using System.Net;
 
 namespace BudgetTracker.Shared.Models;
 
+/// <summary>
+/// Default API response model
+/// </summary>
 public record ApiResponse<T>
 {
+    /// <summary>
+    /// The HTTP status code of the response.
+    /// </summary>
+    /// <example>201</example>
     [JsonPropertyName("status_code")]
     public required HttpStatusCode StatusCode { get; init; } // TODO: Can be changed from System.Net.HttpStatusCode to Microsoft.AspNetCore.Http.StatusCode ?
 
+    /// <summary>
+    /// A unique GUID for tracing the request across services.
+    /// </summary>
+    /// <example>0af7651916cd43dd8448eb211c80319c</example>
     [JsonPropertyName("trace_id")]
     public string TraceId { get; init; } = string.Empty;
 
+    /// <summary>
+    /// Generic message of API operations. (Successfully inserted, Insertion failed) or any exception messages
+    /// </summary>
+    /// <remarks>
+    /// Will be <c>null</c> if the response is successful and there is data in <see cref="Result"/>
+    /// </remarks>
+    /// <example>Transaction inserted successfully</example>
     [JsonPropertyName("message")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
     public string? Message { get; init; }
 
+    /// <summary>
+    /// The payload of the response. This is <c>null</c> if the request fails.
+    /// </summary>
     [JsonPropertyName("result")]
     [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
     public T? Result { get; init; }
