9b31b39068b1d6755035d0b98a903d19fad44cdd
Abhiram13 <abhiramnagadi@gmail.com>
2026-04-25 23:09:59 +0530

    Added common validations in private setters

--- Src/BudgetTracker.Finance/Entities/Categories.Entity.cs
+++ Src/BudgetTracker.Finance/Entities/Categories.Entity.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
 using BudgetTracker.Shared.Entities;
 using BudgetTracker.Shared.Constants;
+using BudgetTracker.Shared.Exceptions;
 
 namespace BudgetTracker.Finance.Entities;
 
@@ -10,17 +12,15 @@ public class Category : BaseEntity
 {
     [Column("name")]
     [JsonPropertyName("name")]
-    [Required(ErrorMessage = "Category Name is required")]
-    [StringLength(maximumLength: SharedConstants.LengthConstants.MAX_CATEGORY_LENGTH, MinimumLength = SharedConstants.LengthConstants.MIN_CATEGORY_LENGTH, ErrorMessage = "Category name exceeds or does not reach required length")]
-    [RegularExpression(SharedConstants.ValidationRegex.NAME_PATTERN, ErrorMessage = "Only letters, numbers, spaces are allowed")]
     public string Name { get; private set; } = string.Empty;
     
     private Category() { }
 
     public static Category Create(string categoryName)
     {
-        Category category = new Category { Name =  categoryName };
+        Validate(categoryName);
         
+        Category category = new Category { Name =  categoryName };
         category.SetModifiedAt();
         
         return category;
@@ -28,8 +28,28 @@ public class Category : BaseEntity
 
     public void Update(string categoryName)
     {
+        Validate(categoryName);
+        
         Name = categoryName;
         
         SetUpdatedAt();
+    }
+    
+    private static void Validate(string categoryName)
+    {
+        if (string.IsNullOrEmpty(categoryName))
+        {
+            throw new InvalidPayloadException("Category name is required");
+        }
+        
+        if (categoryName.Length < SharedConstants.LengthConstants.MIN_CATEGORY_LENGTH || categoryName.Length > SharedConstants.LengthConstants.MAX_CATEGORY_LENGTH)
+        {
+            throw new InvalidPayloadException("Category name exceeds or does not reach required length");
+        }
+
+        if (!Regex.IsMatch(categoryName, SharedConstants.ValidationRegex.NAME_PATTERN))
+        {
+            throw new InvalidPayloadException("Category name contains invalid characters. Only letters, numbers, spaces are allowed");
+        }
     }
 }
