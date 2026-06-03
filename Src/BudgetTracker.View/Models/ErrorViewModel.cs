0329f8be69e05c49146e6d459d81034dbcbcd61c
Abhiram13 <abhiramnagadi@gmail.com>
2026-06-03 21:11:57 +0530

    Razor MVC Init - Also made EFcore tools public from private in finance.csproj

---
+++ Src/BudgetTracker.View/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BudgetTracker.View.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
