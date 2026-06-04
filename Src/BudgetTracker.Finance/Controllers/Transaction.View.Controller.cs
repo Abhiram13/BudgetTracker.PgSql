e4d9f75082788a1aec8572444f319938bca9e7fe
Abhiram13 <abhiramnagadi@gmail.com>
2026-06-04 21:03:35 +0530

    Month and year query params in transactions view controller - Previous month data fetch

--- Src/BudgetTracker.Finance/Controllers/Transaction.View.Controller.cs
+++ Src/BudgetTracker.Finance/Controllers/Transaction.View.Controller.cs
@@ -12,26 +12,39 @@ namespace BudgetTracker.Finance.Mvc.Controllers;
 public class TransactionViewController : Controller
 {
     private readonly TransactionService _transactionService;
+    private readonly ILogger<TransactionViewController> _logger;
     
-    public TransactionViewController(TransactionService transactionService)
+    public TransactionViewController(TransactionService transactionService, ILogger<TransactionViewController> logger)
     {
         _transactionService = transactionService;
+        _logger = logger;
     }
     
-    // [HttpGet]
-    public async Task<IActionResult> Index()
+    [HttpGet]
+    public async Task<IActionResult> Index(int? month, int? year)
     {
-        int month = DateTime.UtcNow.AddMonths(-1).Month;
-        int year = DateTime.UtcNow.Year;
-        int countTask = await _transactionService.CountOfAllTransactionsAsync(month, year);
-        IReadOnlyList<TransactionsListByMonthYear> listTask = await _transactionService.GetTransactionsByMonthYearAsync(month, year);
+        int queryMonth = month ?? DateTime.UtcNow.Month;
+        int queryYear = year ?? DateTime.UtcNow.Year;
+        int countTask = await _transactionService.CountOfAllTransactionsAsync(queryMonth, queryYear);
+        IReadOnlyList<TransactionsListByMonthYear> listTask = await _transactionService.GetTransactionsByMonthYearAsync(queryMonth, queryYear);
         
         TransactionModel model = new TransactionModel
         {
             Count = countTask,
-            Transactions = listTask
+            Transactions = listTask,
+            SelectedMonth = queryMonth,
+            SelectedYear = queryYear
         };
         
         return View("~/Views/Home/Index.cshtml", model);
+    }
+
+    public IActionResult PreviousMonth(int month, int year)
+    {
+        DateTime date = new DateTime(year, month, 1).AddMonths(-1);
+        
+        _logger.LogInformation(date.ToString());
+        
+        return RedirectToAction(nameof(Index), new { month = date.Month, year = date.Year });
     }
 }
--- Src/BudgetTracker.Finance/Models/Transactions.View.Model.cs
+++ Src/BudgetTracker.Finance/Models/Transactions.View.Model.cs
@@ -6,4 +6,6 @@ public record TransactionModel
 {
     public int Count { get; init; }
     public required IReadOnlyList<TransactionsListByMonthYear> Transactions { get; init; }
+    public int SelectedMonth { get; init; }
+    public int SelectedYear { get; init; }
 }
